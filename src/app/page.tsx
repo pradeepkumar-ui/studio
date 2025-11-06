63d76630f121e544bb5760caef342d42856fe5d3
Mari Muthu <marimuthu.p@infinitisoftware.net>
2025-11-06 08:36:27 +0000

    getting 404 This page could not be found. error, pls fix it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-'use client';
-import { redirect } from 'next/navigation';
-import { useEffect } from 'react';
-
 export default function Home() {
-  useEffect(() => {
-    redirect('/dashboard');
-  }, []);
-
   return null;
 }
