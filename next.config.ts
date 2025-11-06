e03e5b3008492ae4e015305e9699a165e0b46c4c
Mari Muthu <marimuthu.p@infinitisoftware.net>
2025-11-06 08:37:49 +0000

    still the same

--- next.config.ts
+++ next.config.ts
@@ -30,15 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/dashboard',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
