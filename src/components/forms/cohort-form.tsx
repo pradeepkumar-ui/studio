f28172a52976da892428c1168c337283204ab47d
Mari Muthu <marimuthu.p@infinitisoftware.net>
2025-11-11 11:20:16 +0000

    ok

--- src/components/forms/cohort-form.tsx
+++ src/components/forms/cohort-form.tsx
@@ -13,7 +13,6 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
 import {
   Select,
   SelectContent,
