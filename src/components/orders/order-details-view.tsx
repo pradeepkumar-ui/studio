dddba0d83badb09c804be377d313042825bcecb1
Mari Muthu <marimuthu.p@infinitisoftware.net>
2025-12-03 05:34:08 +0000

    I'm not seeing any changes in the order details screen

--- src/components/orders/order-details-view.tsx
+++ src/components/orders/order-details-view.tsx
@@ -124,7 +124,7 @@ export function OrderDetailsView({ order }: { order: OrderDetails }) {
     }
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-start mt-6">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 items-start">
         <div className="lg:col-span-2 flex flex-col gap-6">
             <Card>
                 <CardHeader className="flex flex-row items-center justify-between">
